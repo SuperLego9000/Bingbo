da16a1d7690519f1bf92384422596823a5a1384d
SuperLego9000 <82184903+SuperLego9000@users.noreply.github.com>
2022-07-07 13:10:15 -0700

    pretty print console

--- SPEED.py
+++ SPEED.py
@@ -6,6 +6,7 @@ from os import system as sys
 import multiprocessing
 print("[40m[97m")
 sys("cls")
+sys("title Bingbo")
 global threadcount
 threadcount = 0
 maxthreads = int(multiprocessing.cpu_count()*.7)
@@ -15,12 +16,14 @@ maxthreads = int(multiprocessing.cpu_count()*.7)
 
 def rich(user, key):
     for i in range(3):
-        print(f"[35mclaiming points for user: {user}[97m")
+        print(f"[35mchecking points for user:[103m[30m {user} [40m[35m...[40m[30m")
         try:
             subp.run(
-                f"cmd /c python data\BingRewards\BingRewards.py -r -hl -e {user} -p {key} -d chrome"
+                f"cmd /c python data\BingRewards\BingRewards.py -r -hl -e {user} -p {key} -d chrome",
+                stdout=subp.DEVNULL,
+                stderr=subp.STDOUT
             )
-            print(f"[93mclaimed available points for user: {user}[97m")
+            print(f"[93mclaimed available points for user: {user}[40m[30m")
         except:
             print("ERROR>OH GOD WHAT DID YOU DO?")
         global threadcount
@@ -34,9 +37,9 @@ if data == []:
 for _, person in enumerate(data):
     if person[2]:
         while threadcount+1 > maxthreads:
-            wait(1)
+            wait(5)
             print(
-                f"                {person[0].split('@outlook.com')[0].split('@hotmail.com')[0]} waiting for thread")
-        print(f" starting thread ({threadcount+1}/{maxthreads})")
+                f"                [41m{person[0].split('@outlook.com')[0].split('@hotmail.com')[0]} waiting for thread[40m")
+        print(f"[40m[97mstarting thread ({threadcount+1}/{maxthreads})[30m")
         threading.Thread(target=lambda: rich(person[0], person[1])).start()
         threadcount += 1
